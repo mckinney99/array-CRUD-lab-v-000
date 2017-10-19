ca3768d8d08b56a509bdcd920b28ff3d9053da73
Eric McKinney <mckinney99@gmail.com>
2017-10-19 20:40:35 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ array = ["1", "2", "3", "4"]
 end
 
 def add_element_to_end_of_array(array, element)
-array  << "5"
+array << element
+element = "5"
 end
 
 def add_element_to_start_of_array(array, element)
